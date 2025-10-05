1a2d59606edbf2ffa1444faeab1fb0ae6e3d2ec0
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-10-05 23:57:35 +0800

    feat: 初始化TypeORM模块并配置MySQL数据库连接
    refactor: 重命名AppModule为AuthModule并更新相关导入
    
    build: 添加TypeORM和MySQL2依赖
    
    chore: 更新pnpm-lock.yaml文件

---
+++ src/database/mysql.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: '123456',
+      database: 'billdb',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
